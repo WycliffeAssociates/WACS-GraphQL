de0ca60be3c6944325da6932a51b916d425d2332
Craig Oliver <craig.robert.oliver@gmail.com>
2022-02-01 13:07:10 -0500

    formatting

--- WACS_GraphQL/__init__.py
+++ WACS_GraphQL/__init__.py
@@ -5,13 +5,15 @@ import azure.functions as func
 
 from graphene import ObjectType, String, Boolean, Field, Schema, List
 
-HELP_TEXT="Please provide an encoded GraphQL data query, e.g. '{ \"query\": \"query { hello }\"}'"
+HELP_TEXT = "Please provide an encoded GraphQL data query, e.g. '{ \"query\": \"query { hello }\"}'"
+
 
 class Query(ObjectType):
     hello = String()
 
     def resolve_hello(query, info):
         return "Hi there!"
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -23,14 +25,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         data = req.get_json()
     except ValueError:
-        return func.HttpResponse(HELP_TEXT, status_code=400 )
+        return func.HttpResponse(HELP_TEXT, status_code=400)
     if "query" not in data:
-        return func.HttpResponse(HELP_TEXT, status_code=400 )
+        return func.HttpResponse(HELP_TEXT, status_code=400)
 
     # Execute query
     result = schema.execute(data["query"]).to_dict()
 
     # Return result
     return func.HttpResponse(
-            json.dumps(result),
-            mimetype="appication/json")
+        json.dumps(result),
+        mimetype="appication/json")
